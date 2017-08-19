a803a44b95ee00f3b5c2c9abbed985717b93ba55
Joe Heck <heckj@mac.com>
2017-08-19 14:30:08 -0500

    trying out friction to see effects

--- SceneKitPlayground.playground/Contents.swift
+++ SceneKitPlayground.playground/Contents.swift
@@ -7,7 +7,7 @@ import SceneKit
 // Load the SKScene from 'GameScene.sks'
 let scene = SCNScene()
 
-let sceneView = SCNView(frame: CGRect(x:0 , y:0, width: 320, height: 240))
+let sceneView = SCNView(frame: CGRect(x:0 , y:0, width: 320, height: 320))
 sceneView.allowsCameraControl = true
 sceneView.autoenablesDefaultLighting = true
 sceneView.scene = scene
@@ -22,8 +22,9 @@ floor.reflectivity = 0.4
 floor.length = 1000
 floor.width = 1000
 let floorNode = SCNNode(geometry: floor)
-let floorPhysicsBody = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(geometry: SCNBox(width:100, height:0.1, length:100, chamferRadius: 0)))
-floorPhysicsBody.collisionBitMask = 1
+let floorPhysicsBody = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(geometry: SCNBox(width:1000, height:0.01, length:1000, chamferRadius: 0)))
+floorPhysicsBody.allowsResting = true
+//floorPhysicsBody.damping = 1.0
 floorNode.physicsBody = floorPhysicsBody
 scene.rootNode.addChildNode(floorNode)
 
@@ -37,12 +38,12 @@ scene.rootNode.addChildNode(helloNode)
 
 let textPhysicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: helloNode, options: nil))
 
-textPhysicsBody.mass = 3
-textPhysicsBody.contactTestBitMask = 1
-//textPhysicsBody.friction = 2
+textPhysicsBody.mass = 10
+textPhysicsBody.friction = 2
+textPhysicsBody.allowsResting = true
 helloNode.physicsBody = textPhysicsBody
 helloNode.position = SCNVector3(x: 0, y:12, z: 0)
-helloNode.physicsBody?.applyForce(SCNVector3(x: 0, y: 50, z:1), asImpulse: true)
+helloNode.physicsBody?.applyForce(SCNVector3(x: 1, y: 2, z:1), asImpulse: true)
 
 
 
